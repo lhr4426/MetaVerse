1ed1f98541390369467acb034ef10a7cc9468be1
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-07-24 23:27:07 +0900

    [feat] 이동/회전 구현 완료

---
+++ Metaverse/Assets/Scripts/Metaverse/Entity/PlayerController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Metaverse
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Setting")]
+        protected Rigidbody2D _rigidbody;
+
+        [SerializeField] private SpriteRenderer characterRenderer;
+
+        protected Vector2 movementDirection = Vector2.zero;
+        public Vector2 MovementDirection { get { return movementDirection; } }
+
+
+        protected Vector2 lookDirection = Vector2.zero;
+        public Vector2 LookDirection { get { return lookDirection; } }
+
+        private Camera camera;
+
+        [Header("Player Stats")]
+        [SerializeField] private float speed = 1.0f;
+        public float Speed { get { return speed; } }
+
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            characterRenderer = GetComponentInChildren<SpriteRenderer>();
+            camera = Camera.main;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Rotate(LookDirection);
+        }
+
+        private void FixedUpdate()
+        {
+            Movement(MovementDirection);
+        }
+
+
+        private void Rotate(Vector2 direction)
+        {
+            float xyAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            bool isLeft = Mathf.Abs(xyAngle) > 90;
+            characterRenderer.flipX = isLeft;
+        }
+
+        private void Movement(Vector2 direction)
+        {
+            direction = direction * Speed;
+            _rigidbody.velocity = direction;
+        }
+
+        void OnMove(InputValue inputValue)
+        {
+            // Debug.Log("움직이는중");
+            movementDirection = inputValue.Get<Vector2>().normalized;
+        }
+
+        void OnLook(InputValue inputValue)
+        {
+            Vector2 mousePosition = inputValue.Get<Vector2>();
+            Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+            lookDirection = (worldPos - (Vector2)transform.position);
+
+            if (lookDirection.magnitude < .9f)
+            {
+                lookDirection = Vector2.zero;
+            }
+            else
+            {
+                lookDirection = lookDirection.normalized;
+            }
+
+        }
+    }
+}
+
