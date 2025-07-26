8460c8db39409a0b0417a04e08e5c2983ad69031
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-07-26 21:55:51 +0900

    [feat] 캐릭터 커스텀 룸 제작

---
+++ Metaverse/Assets/Scripts/Metaverse/Room/CustomRoom.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Metaverse
+{
+    public class CustomRoom : BaseRoom
+    {
+        public override void Init(UIManager uiManager)
+        {
+            base.Init(uiManager);
+            RoomName = "Ä¿½ºÅÒ ·ë";
+        }
+    }
+}
+
+
